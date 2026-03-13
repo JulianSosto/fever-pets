8b0c493992c86b35ed7bd478dc1e01524b22ce6c
JulianFrancisoSosto <julian.sosto@gmail.com>
2026-03-13 17:29:34 -0300

    0: git init and proyect generated

---
+++ src/app/features/pets/pages/pet-detail/pet-detail.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pet-detail',
+  imports: [],
+  templateUrl: './pet-detail.html',
+  styleUrl: './pet-detail.scss',
+})
+export class PetDetail {
+
+}
---
+++ src/app/features/pets/pages/pets-list/pets-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pets-list',
+  imports: [],
+  templateUrl: './pets-list.html',
+  styleUrl: './pets-list.scss',
+})
+export class PetsList {
+
+}
